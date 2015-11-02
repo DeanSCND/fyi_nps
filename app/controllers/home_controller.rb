1281a005063fd3464140a1aef67d451dcfd712e6
Dean Skelton <dean.skelton@gmail.com>
2015-11-01 18:01:10 -0700

    hack patch for adrian

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController  
   def index
-    @config = Rails.configuration.path
+    #@config = Rails.configuration.path
+    @config = "/Users/Adrian/Work/FYi/Net Promoter Score/"
     puts "PATH: " + @config
   end
 end
